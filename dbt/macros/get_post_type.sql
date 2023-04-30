3dfbe1b6c588f36b7cba3914d3a28de000dd6ac9
Satya Adda <addasaiteja@gmail.com>
2023-04-30 21:56:35 +0000

    fix syntax case when error for post_type macros

--- dbt/macros/get_post_type.sql
+++ dbt/macros/get_post_type.sql
@@ -4,11 +4,11 @@
 
 {% macro get_post_type(post_url) -%}
 
-    case {{ post_url }}
-        when like '%i.redd%' then 'image'
-        when like '%v.redd%' then 'video'
-        when like '%comments%' then 'text'
-        when like '%gallery%' then 'gallery'
+    case
+        when {{ post_url }} like '%i.redd%' then 'image'
+        when {{ post_url }} like '%v.redd%' then 'video'
+        when {{ post_url }} like '%comments%' then 'text'
+        when {{ post_url }} like '%gallery%' then 'gallery'
         else 'external_link'
     end
 
